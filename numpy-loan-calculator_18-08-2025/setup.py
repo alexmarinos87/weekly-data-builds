b088b42abb217470597604541c7fa5fbe90273c1
amarinos10 <alexmarinos967@gmail.com>
2025-08-23 10:28:18 +0100

    Add numpy-loan-calculator module with setup, LICENSE, and tests

---
+++ numpy-loan-calculator_18-08-2025/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup_info = dict(
+    name='numpy-loan-calculator',
+    version=info['version'],
+    author='Alex Marinos',
+    author_email ='alexmarinos967@gmail.com',
+)
