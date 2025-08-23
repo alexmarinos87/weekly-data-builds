20983fb0df241fc2e0e5a719d2ccd215722575c4
amarinos10 <alexmarinos967@gmail.com>
2025-08-23 16:31:01 +0100

    New projects

---
+++ pandas-numpy-time-series_18-2025/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pandas-numpy-time-series_18-2025',
+      version='1.0',
+      description='Conduct a time series analysis on interest rates how to they affect foreign exchange using pandas and numpy',
+      author='Alex Marinos',
+      author_email='alexmarinos967@gmail.com',
+      url='',
+      packages=['distutils', 'distutils.command'],
+     )
